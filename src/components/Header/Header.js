75b53266d363090c3db7ddb03f446333385f0c9a
Luiz Roberto Da Cruz Santos <luizsantos.20lrs@gmail.com>
2019-06-10 11:21:39 -0300

    primeira versão do Menu

---
+++ src/components/Header/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Header.css';
+import './boot.css';
+import logo from './logo.jpg';
+
+
+
+
+const header =() => { 
+	return < div class= "container-ms"> 
+
+
+					<nav class = " nav justify-content-end "> 
+					<img src={logo} id ="logo" / >
+								<ul class="nav justify-content-end">
+									  <li class="nav-item">
+									    <a class="nav-link active" href="#">Cooperflora</a>
+									  </li>
+									  <li class="nav-item">
+									    <a class="nav-link" href="#">Localização</a>
+									  </li>
+									  <li class="nav-item">
+									    <a class="nav-link" href="#">Cadastro</a>
+									  </li>
+									  <li class="nav-item">
+									    <a class="nav-link" href="#">O projeto</a>
+									  </li>
+								</ul>
+
+						<button type="button" class="btn btn-success"> Entrar</button>
+
+
+					</nav>
+		</div>
+
+
+
+}; 
+
+
+export default header; 
