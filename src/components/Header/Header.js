7d4372a6c12eab55f08d7985864fd31b68d52fe4
André Luís Bordignon <albordignon@gmail.com>
2019-06-17 08:36:35 -0300

    Configurando ESLint

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,38 +3,28 @@ import './Header.css';
 import './boot.css';
 import logo from './logo.jpg';
 
-
-
-
 const header =() => { 
+	
 	return < div class= "container-ms"> 
-
-
-					<nav class = " nav justify-content-end "> 
-					<img src={logo} id ="logo" / >
-								<ul class="nav justify-content-end">
-									  <li class="nav-item">
-									    <a class="nav-link active" href="#">Cooperflora</a>
-									  </li>
-									  <li class="nav-item">
-									    <a class="nav-link" href="#">Localização</a>
-									  </li>
-									  <li class="nav-item">
-									    <a class="nav-link" href="#">Cadastro</a>
-									  </li>
-									  <li class="nav-item">
-									    <a class="nav-link" href="#">O projeto</a>
-									  </li>
-								</ul>
-
-						<button type="button" class="btn btn-success"> Entrar</button>
-
-
-					</nav>
-		</div>
-
-
-
+	<nav class = " nav justify-content-end "> 
+	<img src={logo} id ="logo" alt="Logo da Cooperativa"/ >
+	<ul class="nav justify-content-end">
+	<li class="nav-item">
+	<a class="nav-link active" href="#">Cooperflora</a>
+	</li>
+	<li class="nav-item">
+	<a class="nav-link" href="#">Localização</a>
+	</li>
+	<li class="nav-item">
+	<a class="nav-link" href="#">Cadastro</a>
+	</li>
+	<li class="nav-item">
+	<a class="nav-link" href="#">O projeto</a>
+	</li>
+	</ul>
+	<button type="button" class="btn btn-success"> Entrar</button>
+	</nav>
+	</div>
 }; 
 
 
