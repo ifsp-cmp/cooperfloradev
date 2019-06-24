6cb643b8bfbbaf06cf41e14bcaa034202ca57f35
Andre Luís Bordignon <albordignon@gmail.com>
2019-06-24 10:46:08 -0300

    Adicionado o home

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import  Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
    
     <Header/> 
+    <Home/>
     <Footer/>
      
     </div>
---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+const home = () => {
+	return (
+		<div className='Home'>
+			<p>Home</p>	
+		</div>
+	);
+}
+
+export default home;
