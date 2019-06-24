53768357b3052b3d0c231846b4102701393facd9
gabriela <gabicosta2015.gc76@gmail.com>
2019-06-24 11:55:52 -0300

    atualização do footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import "./Footer.css";
+import logoInstagram from "../../assets/images/instagram.png";
+import logoFacebook from "../../assets/images/facebook.png";
 
 const footer =() => { 
 	return(
-		<div className="Footer">
-			<p>Teste de Rodapé</p>
+		<div class="Footer">
+			<img id="insta" src={logoInstagram}/>
+			<img id="face" src={logoFacebook}/>
 		</div>
 	)
 };
