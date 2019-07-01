72e4785447fdab8cb43130d048360c778f6cde58
gabriela <gabicosta2015.gc76@gmail.com>
2019-07-01 12:04:02 -0300

    finalizado footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import  Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 
 
 function App() {
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import "./Footer.css";
+import './boot.css';
 import logoInstagram from "../../assets/images/instagram.png";
 import logoFacebook from "../../assets/images/facebook.png";
+import logoGmail from "../../assets/images/gmail.png";
 
 const footer =() => { 
 	return(
-		<div class="Footer">
-			<img id="insta" src={logoInstagram}/>
-			<img id="face" src={logoFacebook}/>
+		<div class="Footer" >
+			<div className="row">
+				<div className="col-sm">
+					<img id="gmail" src={logoGmail}/> <p>contatocooperflora@gmail.com</p>
+				</div>
+				<div className="col-sm">
+					<p id="licen">Licenciado XXX</p>
+				</div>
+				<div className="col-sm">
+					<img id="insta" src={logoInstagram}/>
+					<img id="face" src={logoFacebook}/>
+				</div>
+			</div>
 		</div>
 	)
 };
