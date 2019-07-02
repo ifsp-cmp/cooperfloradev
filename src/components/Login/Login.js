985ed0f0bf0886f7542445bcc0c2b59af596d718
gabriela <gabicosta2015.gc76@gmail.com>
2019-07-02 12:11:47 -0300

    alteração na página de login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import "./Login.css";
 import '../Footer/boot.css';
+import PageLayout from '../../components/PageLayout/PageLayout';
+
 
 const login =() => { 
 	return(
-	<form className="login" >
-		<label for="email">E-mail: <input id="email" type="text" name="email"/></label><br/>
-		<label for="senha">Senha: <input id="senha" type="password" name="senha"/></label><br/>
-		<input type="checkbox" name="conect" value="sim"/>
-		<label for="conect">Manter-me conectado.</label>
-		<input type="submit" name="Enviar" value="Enviar"/> 
-		<input type="reset" name="Cancelar" value="Cancelar"/>
-	</form>
+	<PageLayout>
+		<form className="login" >
+			<label for="email">E-mail: <input id="email" type="text" name="email"/></label><br/>
+			<label for="senha">Senha: <input id="senha" type="password" name="senha"/></label><br/>
+			<input type="checkbox" name="conect" value="sim"/>
+			<label for="conect">Manter-me conectado.</label><br/><br/>
+			<input type="submit" name="Enviar" value="Enviar" id="enviar"/> 
+			<input type="reset" name="Cancelar" value="Cancelar" id="cancelar"/>
+		</form>
+	</PageLayout>
 			
 	
 	)
