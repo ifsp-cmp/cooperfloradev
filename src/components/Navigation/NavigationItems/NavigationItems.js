c7d094305e5930734b48e4cdc4d77044700b10f9
André Luís Bordignon <albordignon@gmail.com>
2019-08-30 08:30:51 -0300

    Adicionado o menu para ceular

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -7,7 +7,13 @@ import './NavigationItems.css'
 const navigationItems = () => (
     <ul className='NavigationItems'>
         <NavigationItem link='/' active>Home</NavigationItem>
+        <NavigationItem link='/cooperflora'>Cooperflora</NavigationItem>
         <NavigationItem link='/projeto'>O projeto</NavigationItem>
+        <NavigationItem link='/listarusuarios'>Listar</NavigationItem>
+        <NavigationItem link='/training'>Formação</NavigationItem>
+        <NavigationItem link='/cadastro'>Cadastro</NavigationItem>
+        <NavigationItem link='/login'>Login</NavigationItem>
+        <NavigationItem link='/logout'>Logout</NavigationItem>
     </ul>
 );
 
